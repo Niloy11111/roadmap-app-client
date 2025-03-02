e6557981248c8df5fef309b4039b5a252e862420
ekhthiar_uddin <ektiaruddinniloy859@gmail.com>
2025-03-02 19:56:42 +0600

    added favicon

--- src/components/layout/RootLayout.tsx
+++ src/components/layout/RootLayout.tsx
@@ -1,13 +1,14 @@
+import Favicon from "react-favicon";
 import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../ui/navbar/Navbar";
 import Footer from "../ui/pages/home/footer/Footer";
-
 const RootLayout = () => {
   const { pathname } = useLocation();
   const hideNavbarPaths = ["/login", "/change-password", "/register"];
 
   return (
     <div>
+      <Favicon url="https://i.ibb.co/Pxf1xtZ/psd-smiling-3d-receptionist-isolated-transparent-background-753500-460-removebg-preview.png" />
       <div className="w-5/7 mx-auto font-Inter text-[#000000]">
         {!hideNavbarPaths.includes(pathname) && <Navbar />}
         <Outlet />
